f01253c2119a9535cc4c70353020ee2cd0feaf70
Kerrick Staley <kerrick@kerrickstaley.com>
2016-06-18 16:28:39 -0700

    Call into asm file

--- src/GcController/GcController.ino
+++ src/GcController/GcController.ino
@@ -13,6 +13,8 @@
   by Scott Fitzgerald
  */
 
+#include <GcControllerLib.h>
+
 
 // the setup function runs once when you press reset or power the board
 void setup() {
@@ -23,7 +25,7 @@ void setup() {
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);              // wait for a second
+  delay(blinkTime());              // wait for a second
   digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);              // wait for a second
 }
---
+++ src/GcControllerLib/GcControllerLib.h
@@ -0,0 +1,8 @@
+#ifndef GC_CONTROLLER_LIB_H
+#define GC_CONTROLLER_LIB_H
+
+extern "C" {
+  uint16_t blinkTime();
+}
+
+#endif // GC_CONTROLLER_LIB_H
